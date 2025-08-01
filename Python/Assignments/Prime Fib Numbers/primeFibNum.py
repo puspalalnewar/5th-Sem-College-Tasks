e9baac2a4ea4cba43209b559d811d7afeabfca38
Puspalal <puspalalnewar79@gmail.com>
2025-08-01 22:18:04 +0530

    Prime Fib Number

--- Python/Assignments/Prime Fib Numbers/primeFibNum.py
+++ Python/Assignments/Prime Fib Numbers/primeFibNum.py
@@ -1,10 +1,9 @@
 # ! Input a limit n and print all prime fibonacci numbers up to n using a user defined function prime() which returns a 1 if the argument is a prime or else 0.
 
-
-input = int(input("Enter your number : "))
-
 # ! Functuon to check Prime number
 def isPrime(num) :
+    if num == 0 :
+        return 0
     if num == 1 :
         return 0
     for i in range (2, num) :
@@ -12,5 +11,20 @@ def isPrime(num) :
             return 0
     return 1  
     
+n = int(input("Enter how many Fibonacci numbers you want: "))
 
-    
+a, b = 0, 1
+fib_list = []
+
+for _ in range(n):
+    fib_list.append(a)
+    a, b = b, a + b
+
+fib_tuple = tuple(fib_list)
+
+print("Fibonacci series as tuple:")
+print(fib_tuple)
+
+for i in range(len(fib_tuple)) :
+    if isPrime(fib_tuple[i]) == 1 :
+        print(fib_tuple[i])  
