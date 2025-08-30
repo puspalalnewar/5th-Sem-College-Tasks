f047b03c2f2aee18bf983c4afddc4418a5180efb
Puspalal <puspalalnewar79@gmail.com>
2025-08-30 17:10:30 +0530

    Get First Letter

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,11 @@
-let myName = "Puspalal Newar Tirtha "
+let myName = "Puspalal Newar Tirtha"
 const arr = myName.split(" ");
 console.log(arr)
+
 let firstEle = arr.map((ele)=> {
     return ele[0]
 })
+
 let convertToStr = firstEle.join("")
 
 console.log(convertToStr)
