52d085054f8ad11ec377df4259c062f95286b453
Puspalal <puspalalnewar79@gmail.com>
2025-09-15 13:34:45 +0530

    Quadratic Equation

--- Python/exercise.py
+++ Python/exercise.py
@@ -8,44 +8,14 @@ import math
 
 # ! Q8. Solve a given quadratic equation. (Without imaginary root).
 
-# ! Q9. Make Star Diamond
+# ax^2 + bx + c = 0
 
-#             *     
-#           * * *
-#         * * * * *
-#       * * * * * * *
-#         * * * * *
-#           * * *
-#             *
+a,b, c = map(int, input("Enter value of a, b, c seperated via space : ").split(" "))
+print(a,b,c)
 
-# row = 5
-# for i in range (1, row+1) :
-#     print(" " * (row - i) + ("* ") * i)
-# # Lower part of the diamond
-# for i in range(row - 1, 0, -1):
-#     print(" " * (row - i) + "* " * i)
+# x = -b + rootover(b^2 - 4ac) / 2a
 
-# ! Q10. Make star hourglass
-
-#         * * * * * * *
-#           * * * * *
-#             * * *
-#               *
-#             * * *
-#           * * * * *
-#         * * * * * * *
-
-# row = 4
-# # For Upper Part
-# for i in range(row, 0, -1) :
-#     print(" " * (row-i) + "* " * i)
-# #For lower part
-# for i in range(2, row+1) :
-#     print(" " * (row-i) + "* " * i)
-
-# ! Q11. Input two numbers and find their hcf and lcm
-# num1 = int(input("Enter num1: "))
-# num2 = int(input("Enter num2 : "))
-# hcf = math.gcd(num1, num2)
-# lcm = (num1 * num2) // hcf
-# print(f"LCM : {lcm}\nHCF : {hcf}")
+if b**2 - 4 * a * c == 0 :
+    print("Roots are Imaginary")
+else :
+    print(f"Roots of equations are : ({(-b + math.sqrt(b**2 - 4*a*c)) / (2*a)}, {(-b - math.sqrt(b**2 - 4*a*c)) / (2*a)})")
